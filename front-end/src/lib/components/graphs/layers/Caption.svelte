b1b287e0ed35f18bcbb59e36a13382fdc9384da1
fsorodrigues <fsorodrigues@gmail.com>
2024-03-13 22:18:23 +1300

    Caption size and elements

--- front-end/src/lib/components/graphs/layers/Caption.svelte
+++ front-end/src/lib/components/graphs/layers/Caption.svelte
@@ -5,9 +5,25 @@
 </script>
 
 <div class="caption caption-{type}">
-    <span class='caption-text'>{ caption }</span>
+    <p class='caption-text'>{@html caption}</p>
     <a class="download-button" href={url} download>
-        Download data <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+        Download data 
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="2" 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                class="feather feather-download"
+            >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                <polyline points="7 10 12 15 17 10"></polyline>
+                <line x1="12" y1="15" x2="12" y2="3"></line>
+            </svg>
     </a>
 </div>
 
@@ -15,10 +31,27 @@
     .caption {
         line-height: 1.65;
         margin: 15px 0;
+        @include fs-base;
     }
 
     .caption-text {
-        display: block
+        display: block;
+
+        :global(.line) {
+            display: inline-block;
+            height: 0.1px;
+            width: 12.5px;
+            border: 1px solid $black;
+            position: relative;
+            top: -2.5px;
+        }
+
+        :global(.area) {
+            display: inline-block;
+            height: 8px;
+            width: 8px;
+            background-color: $black;
+        }
     }
 
     .download-button {
