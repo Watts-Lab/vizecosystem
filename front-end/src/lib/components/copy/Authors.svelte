64e1e0ba9e77ab2775558174dbf9a6c4858c3caa
fsorodrigues <fsorodrigues@gmail.com>
2024-06-16 21:04:07 +1200

    Add new credit section

--- front-end/src/lib/components/copy/Authors.svelte
+++ front-end/src/lib/components/copy/Authors.svelte
@@ -14,9 +14,10 @@
 </script>
 
 <div class='line'></div>
-<div class='title'>Credits</div>
+<div class='title'>The Team</div>
 <div class='authors'>
 	{#each authors as author, i}
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
 		<div 
 			class='author-container'
 			on:mouseover={handleMouseOver}
@@ -24,7 +25,11 @@
 			on:mouseout={handleMouseOut}
 			on:blur={handleMouseOut}
 		>
-			<p class='author-name'>{i === length-1 ? 'and ' : ''}{author.name}{i !== length-1 ? ',' : ''}</p>
+			<div class='author-photo' style="--photo-url: url('/assets/images/{author.photo}')"></div>
+			<a class='author-bio' href={author.url}>
+				<p class='author-name'>{author.name}</p>
+				<p class='author-desc'>{author.desc}</p>
+			</a>
 		</div>
 	{/each}
 </div>
@@ -37,22 +42,64 @@
 		grid-row: 3 / span 1;
 		display: flex;
 		flex-wrap: wrap;
-		column-gap: 5px;
-		row-gap: 2px;
+		column-gap: 10px;
+		row-gap: 8px;
 
 		@media (min-width: $bp-3) {
 			// @include grid-main;
 			grid-row: 3 / span 1;
-			grid-column: 3 / span 5;
+			grid-column: 3 / span 8;
 		}
 
 		.author-container {
 			display: inline-flex;
-			align-items: baseline;
+			// align-items: baseline;
 			margin: 0;
 			position: relative;
+			width: 100%;
+			column-gap: 5px;
+
+			@media (min-width: $bp-1) {
+				width: 32%;
+			}
+
+			.author-photo {
+				width: 45px;
+				height: 45px;
+				background-size: cover;
+				background-position: center;
+				background-image: var(--photo-url);
+				border-radius: 50%;
+				flex-grow: 0;
+				flex-shrink: 0;
+			}
+
+			.author-bio {
+				display: grid;
+				grid-template-columns: 1fr;
+				grid-template-rows: 0.3fr 0.7fr;
+				color: $black;
+			}
+
+			.author-bio:hover {
+				opacity: 0.9;
+			}
+			
+			.author-bio:visited {
+				color: $black;
+			}
 
 			.author-name {
+				font-weight: 500;
+				display: inline;
+				pointer-events: none;
+				@include fs-sm;
+
+				@media (min-width: $bp-3) {
+					@include fs-root;
+				}
+			}
+			.author-desc {
 				font-weight: 300;
 				display: inline;
 				pointer-events: none;
@@ -62,14 +109,13 @@
 					@include fs-sm;
 				}
 			}
+			// .author-name:after {
+			// 	content: ',';
 
-			.author-name:after {
-				content: ',';
-
-				@media (min-width: $bp-3) {
-					display: none;
-				}
-			}
+			// 	@media (min-width: $bp-3) {
+			// 		display: none;
+			// 	}
+			// }
 
 			.detail {
 				display: none;
@@ -104,19 +150,29 @@
 	}
 
 	.line {
-		grid-row: 1 / span 1;
-		grid-column: 3 / span 8;
+		grid-column: 1 / span 8;
 		width: 25%;
 		height: 1px;
 		background-color: $black;
 		margin: 10px 0;
+		
+		@media (min-width: $bp-3) {
+			grid-row: 1 / span 1;
+			grid-column: 3 / span 8;
+		}
+
 	}
+
 	.title {
-		grid-row: 2 / span 1;
-		grid-column: 3 / span 8;
+		grid-column: 1 / span 8;
 		@include fs-root;
 		font-weight: 700;
-		margin-bottom: 5px;
+		margin-bottom: 10px;
 		// text-transform: uppercase;
+
+		@media (min-width: $bp-3) {
+			grid-row: 2 / span 1;
+			grid-column: 3 / span 8;
+		}
 	}
 </style>
